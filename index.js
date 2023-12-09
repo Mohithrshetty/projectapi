ff3e6662487bb851ce5efe2b3971aebd2febcd8b
Sujith <sujithsuji1098@gmail.com>
2023-12-09 22:17:52 +0530

    added admin listing page and donar email code "

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-const path = require('path')
+const path = require("path");
 
-require('./config/db');
+require("dotenv").config();
+require("./config/db");
 
-app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname, 'public')))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 const cors = require("cors");
+const Donate = require("./models/donate");
+
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.render('charity.ejs')
-})
+  res.render("charity.ejs");
+});
 
-app.get("/donate", (req, res) => {
-    res.render('donate.ejs')
-})
+app.get("/donate-page", (req, res) => {
+  res.render("donate.ejs");
+});
 
-app.use("/user", require("./routes/user.route"))
-app.use("/donate", require("./routes/donate.route"))
+app.get("/admin", async (req, res) => {
+  const donations = await Donate.find();
+  res.render("admin.ejs", { data: donations });
+});
+
+app.use("/user", require("./routes/user.route"));
+app.use("/donate", require("./routes/donate.route"));
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running on localhost:${PORT}`);
-})
+  console.log(`Server is up and running on localhost:${PORT}`);
+});
--- routes/donate.route.js
+++ routes/donate.route.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { nodeMailer } = require("../helpers/node-mailer");
 const Donate = require("../models/donate");
 
 // Route to get all donations
@@ -14,11 +15,23 @@ router.get("/", async (req, res) => {
 // Route to create a new donation
 router.post("/", async (req, res) => {
   const newDonation = new Donate(req.body);
-
+  console.log(req.body);
   try {
-    const savedDonation = await newDonation.save();
-    res.status(201).json({message: "Donation Successfull"});
+    await newDonation.save();
+
+    let mailDetails = {
+      from: process.env.MAIL_FROM,
+      to: req.body.donorEmail,
+      subject: "Thank you for your help!...",
+      html: `Thank you for the donation`,
+    };
+
+    const mailResponse = await nodeMailer.sendMail(mailDetails);
+    if (mailResponse) {
+      res.status(201).json({ message: "Donation Successfull" });
+    }
   } catch (error) {
+    console.log(error);
     res.status(400).json({ message: error.message });
   }
 });
